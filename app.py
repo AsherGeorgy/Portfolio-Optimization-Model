34be88b1670e9304052fb4a4b34cc0a3e174b886
AsherGeorgy <georgy.asher@gmail.com>
2024-12-06 18:08:21 +0530

    Updated UI and added sidebar disclaimer
    - Updated the header and description styling.
    - Added a sidebar with developer info and disclaimer.
    - Updated the separator line style.
    - Enhanced the portfolio configuration section with custom styling.

--- app.py
+++ app.py
@@ -4,21 +4,47 @@ import utils.optimization as opt
 import utils.visualization as vis
 
 # Header section
-st.markdown(
-    "<h1 style='text-align:center; color:#4CAF50;'>Portfolio Optimization Model</h1>",
-    unsafe_allow_html=True,
-)
+st.markdown("<h1 style='text-align:center; color:#003366;'>Portfolio Optimization Model</h1>", unsafe_allow_html=True)
 st.markdown(
     "<p style='text-align:center; font-size: 18px;'>This is a Python-based application for analyzing and optimizing financial portfolios. "
     "It uses modern portfolio theory and convex optimization techniques to identify optimal allocations, backtest performance, and visualize results.</p>",
     unsafe_allow_html=True,
 )
+st.markdown(
+    "<p style='text-align:center; font-size: 15px;'>"
+    "<a href='https://github.com/AsherGeorgy/portfolio-optimization-tool/tree/streamlit_app' target='_blank'>View Source Code on GitHub</a>"
+    "</p>",
+    unsafe_allow_html=True,
+)
+
+
+# Sidebar disclaimer
+with st.sidebar:
+    # Developer Information
+    st.markdown("<h1 style='color: #003366;'>Developed by:</h1>", unsafe_allow_html=True)
+    st.markdown("<hr style='border: 0; border-top: 2px solid #003366; margin-top: 0px; margin-bottom: 10px;'>", unsafe_allow_html=True)  # Underline-style line below the heading
+    st.markdown("<h3 style='font-weight: bold; color: #333;'>Asher Georgy</h3>", unsafe_allow_html=True)
+    st.markdown("<p style='color: #555;'>Finance professional turned data enthusiast. <br><a href='https://ashergeorgy.github.io/' target='_blank' style='color: #007BFF; text-decoration: none;'>Visit my Website</a></p>", unsafe_allow_html=True)
+
+    # Disclaimer Section
+    st.markdown("<h1 style='color: #003366;'>Disclaimer:</h1>", unsafe_allow_html=True)
+    st.markdown("<hr style='border: 0; border-top: 2px solid #003366; margin-top: 0px; margin-bottom: 10px;'>", unsafe_allow_html=True)  # Underline-style line below the heading
+    st.markdown("""
+    <div style="color: #555; font-size: 14px;">
+        <p>This application is for educational and informational purposes only. The analysis, data, and results provided by this tool are based on historical data and theoretical models. 
+        <u><strong>They do not predict future performance.</strong></u></p>
+        <p>Users should conduct their own due diligence, verify results, and consult a qualified financial advisor before making any investment decisions.</p>
+    </div>
+    """, unsafe_allow_html=True)
+
+    # Link to full disclaimer
+    st.markdown("<p style='font-size: 13px; text-align: center; margin-top: 20px;'><a href='https://ashergeo/full-disclaimer' target='_blank' style='color: #007BFF; text-decoration: none;'>View full Disclaimer and Limitations</a></p>", unsafe_allow_html=True)
 
 # Separator line
-st.markdown("<hr style='border: 1px solid #4CAF50;'>", unsafe_allow_html=True)
+st.markdown("<hr style='border: 1px solid #003366;'>", unsafe_allow_html=True)
 
 # Custom input fields
-st.markdown("### Configure Your Portfolio:")
+st.markdown("<h3 style='color: #003366;'>Configure Your Portfolio:</h3>", unsafe_allow_html=True)
 user_input = st.text_input(
     "Enter stock tickers separated by commas: ",
     placeholder="aapl, msft, tsla",
@@ -119,4 +145,8 @@ if assets_list is not None:
 
 else:
     None 
+
+
+
+
 
